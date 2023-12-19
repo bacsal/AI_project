a802d71134ab8c6f7ae907f40a0aefb26d6719fc
bacsal <50982614+bacsal@users.noreply.github.com>
2023-12-19 12:39:46 +0900

    set.py
    set.py

---
+++ Scouter/set.py
@@ -0,0 +1,11 @@
+import os
+import pandas as pd
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+csv_file_path = os.path.join(script_dir, 'result.csv')
+df = pd.read_csv(csv_file_path, encoding='cp949')
+setname = set()
+for i in df['MY_CHAMPION']:
+    setname.add(i)
+name = pd.DataFrame(setname)
+name.to_csv("name.csv")
